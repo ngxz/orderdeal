0c86986c99b52dc7055ec378887877e66287d8dd
ngxz <ngxz92@163.com>
2017-12-20 10:02:10 +0800

    function里判断，跳转手机视图Mobile

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -8,6 +8,13 @@ class PublicController extends Controller{
         if (!$_SESSION['loginid']){
             redirect(U('Home/Login/login'));
         }
+        //移动设备浏览，则切换模板
+        if (ismobile()) {
+            //设置默认默认主题为 Mobile
+//          C('DEFAULT_THEME','Mobile');
+			//设置默认默认视图为 Mobile
+            C('DEFAULT_V_LAYER','Mobile');
+        }
     }
 }
 
